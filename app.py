8d26ef69430cbbde89fc1360ce9f864906504b92
Saiyam Pathak <saiyam911@gmail.com>
2024-02-10 20:18:33 +0530

    fix

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 model_id = "runwayml/stable-diffusion-v1-5"
 
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32, low_cpu_mem_usage=True)
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32, low_cpu_mem_usage=True, safety_checker=None)
 
 @app.route("/v1/models/serving:predict", methods=["POST"])
 def predict():
